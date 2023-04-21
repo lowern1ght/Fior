40b576a4a353ae59bd81bba40d6d2b76f24d8db5
Alexander Sergeev <bradwoord@gmail.com>
2023-04-22 01:36:21 +0500

    re folder, add project console in shared folder

---
+++ FiorConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using FiorSearchService;
+using FiorSearchService.Realization;
+
+namespace FiorConsole {
+    internal class Program {
+        private static Task InitializeLogger() {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Debug()
+                .CreateLogger();
+            return Task.CompletedTask;
+        }
+
+        static async Task Main() {
+            await InitializeLogger();
+            var service = new GoogleSearch(new() { 
+                ApiKey = "AIzaSyDBRc-mwzyEgSpc0fq1nWbUmKQH_ZOQimY",
+                Cx = "b0edae207179a4dd3",
+                ElementCount = 10,
+            });
+
+            var result = await service.SearchAsync("Лампа SWEKO 20W");
+            if (result is not null) Parallel.ForEach(result, d => Log.Information("Title: [{0}] [{1}]",
+                d.Title, d.Snippet));
+            else throw new ArgumentNullException(nameof(result));
+        }
+    }
+}
