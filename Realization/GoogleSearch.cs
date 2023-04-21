10f70244ddc547759c98e20c3a6ad8ab62f0f2b0
Alexander Sergeev <bradwoord@gmail.com>
2023-04-22 01:18:48 +0500

    ready method

--- Realization/GoogleSearch.cs
+++ Realization/GoogleSearch.cs
@@ -1,16 +1,11 @@
-﻿
-using Google.Apis.Services;
-using Google.Apis.CustomSearchAPI;
-using Google.Apis.CustomSearchAPI.v1;
+﻿using Google.Apis.CustomSearchAPI.v1;
 
 namespace FiorSearchService.Realization;
 
 public record class GoogleSearch : SearchService {
-    public new GoogleServiceConfig ServiceConfig { get; init; }
     public CustomSearchAPIService CustomSearch { get; init; }
 
-    public GoogleSearch(GoogleServiceConfig serviceConfig) {
-        ServiceConfig = serviceConfig;
+    public GoogleSearch(SearchServiceConfig serviceConfig) : base(serviceConfig) {
         CustomSearch = new (
             new Google.Apis.Services.BaseClientService.Initializer() { 
                 ApiKey = ServiceConfig.ApiKey,
@@ -19,20 +14,13 @@ public record class GoogleSearch : SearchService {
 
     public override async Task<IEnumerable<Google.Apis.CustomSearchAPI.v1.Data.Result>?> SearchAsync(string textSearch) {
         var listRequare = CustomSearch.Cse.List();
-        listRequare.Num = this.ServiceConfig.NumPageSize;
-        listRequare.Cx = ServiceConfig.CxId ?? throw new ArgumentNullException(nameof(ServiceConfig.CxId));
+        listRequare.Num = this.ServiceConfig.ElementCount;
+        listRequare.Cx = ServiceConfig.Cx ?? throw new ArgumentNullException(nameof(ServiceConfig.Cx));
         listRequare.Q = textSearch;
 
         var result = await listRequare.ExecuteAsync();
         if (result is null) 
             throw new ArgumentNullException(nameof(result));
         return result.Items;
-    }
-
-    public record struct GoogleServiceConfig {
-        public String? CxId { get; set; }
-        public String? OAuth2 { get; set; } 
-        public String? ApiKey { get; set; }
-        public UInt16? NumPageSize { get; set; }
     }
 }
--- SearchService.cs
+++ SearchService.cs
@@ -1,8 +1,18 @@
 ﻿namespace FiorSearchService;
 
 public abstract record class SearchService {
-    public virtual Object ServiceConfig { get; init; }
+    public virtual SearchServiceConfig ServiceConfig { get; init; }
 
     public virtual Task SearchAsync(String textSearch)
         => throw new NotImplementedException();
+
+    public SearchService(SearchServiceConfig serviceConfig) {
+        ServiceConfig = serviceConfig;
+    }
+}
+
+public record class SearchServiceConfig {
+    public String? Cx { get; set; }
+    public String? ApiKey { get; set; }
+    public UInt16? ElementCount { get; set; }
 }
