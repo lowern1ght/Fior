002f51493d4c1d0ed19db46429a8a20fd1d8bd0f
Alexander Sergeev <bradwoord@gmail.com>
2023-04-22 13:10:29 +0500

    add answer console read line

--- FiorConsole/Program.cs
+++ FiorConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Serilog;
 using FiorSearchService;
 using FiorSearchService.Realization;
+using Pastel;
 
 namespace FiorConsole {
     internal class Program {
@@ -12,15 +13,28 @@ namespace FiorConsole {
             return Task.CompletedTask;
         }
 
+        private static async Task WriteHeader() {
+            await Console.Out.WriteLineAsync(Resources.ConsoleResources.ANSIHeader);
+        }
+
         static async Task Main() {
             await InitializeLogger();
+            await WriteHeader();
+
+            await Console.Out.WriteLineAsync("");
+            String? answer = Console.ReadLine();
+            if (answer is null) {
+                await Console.Out.WriteLineAsync("Answer".Pastel(ConsoleColor.DarkRed) + " is empty string!");
+                Environment.Exit(0);
+            }
+
             var service = new GoogleSearch(new() { 
                 ApiKey = "AIzaSyDBRc-mwzyEgSpc0fq1nWbUmKQH_ZOQimY",
                 Cx = "b0edae207179a4dd3",
                 ElementCount = 10,
             });
 
-            var result = await service.SearchAsync("Лампа SWEKO 20W");
+            var result = await service.SearchAsync(answer);
             if (result is not null) Parallel.ForEach(result, d => Log.Information("Title: [{0}] [{1}]",
                 d.Title, d.Snippet));
             else throw new ArgumentNullException(nameof(result));
