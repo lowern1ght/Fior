69cbfdb23af35f0a0aa805843aa157abec31da34
Alexander Sergeev <bradwoord@gmail.com>
2023-04-22 13:51:04 +0500

    #decoration

--- FiorConsole/Program.cs
+++ FiorConsole/Program.cs
@@ -14,29 +14,32 @@ namespace FiorConsole {
         }
 
         private static async Task WriteHeader() {
-            await Console.Out.WriteLineAsync(Resources.ConsoleResources.ANSIHeader);
+            await Console.Out.WriteLineAsync(Resources.ConsoleResources.ANSIHeader.Pastel(System.Drawing.Color.Black) 
+                + Environment.NewLine);
         }
 
         static async Task Main() {
             await InitializeLogger();
             await WriteHeader();
 
-            await Console.Out.WriteLineAsync("");
+            await Console.Out.WriteAsync("  Write search string: ".Pastel(ConsoleColor.Gray) 
+                + Environment.NewLine + "    > ".Pastel(ConsoleColor.Green));
             String? answer = Console.ReadLine();
             if (answer is null) {
                 await Console.Out.WriteLineAsync("Answer".Pastel(ConsoleColor.DarkRed) + " is empty string!");
                 Environment.Exit(0);
             }
 
-            var service = new GoogleSearch(new() { 
+            var service = new GoogleSearch(new() {
                 ApiKey = "AIzaSyDBRc-mwzyEgSpc0fq1nWbUmKQH_ZOQimY",
                 Cx = "b0edae207179a4dd3",
                 ElementCount = 10,
             });
 
+            Console.Clear();
             var result = await service.SearchAsync(answer);
-            if (result is not null) Parallel.ForEach(result, d => Log.Information("Title: [{0}] [{1}]",
-                d.Title, d.Snippet));
+            if (result is not null) Parallel.ForEach(result, async d => 
+                await Console.Out.WriteLineAsync($"Title: [{d.Title.Pastel(System.Drawing.Color.BlueViolet)}] [{d.Snippet.Pastel(ConsoleColor.Gray)}]"));
             else throw new ArgumentNullException(nameof(result));
         }
     }
