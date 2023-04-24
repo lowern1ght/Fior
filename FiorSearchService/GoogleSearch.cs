c83e8dadddb8b483fe6b1d868224db54daba06b8
Alexander Sergeev <bradwoord@gmail.com>
2023-04-25 00:38:31 +0500

    #10ap1

--- FiorSearchService/GoogleSearch.cs
+++ FiorSearchService/GoogleSearch.cs
@@ -6,6 +6,9 @@ using System.Text.Encodings.Web;
 using Newtonsoft.Json.Linq;
 using System.Text.Unicode;
 using CsQuery;
+using CsQuery.ExtensionMethods.Internal;
+using CsQuery.ExtensionMethods;
+using Google.Apis.CustomSearchAPI.v1.Data;
 
 namespace FiorSearchService;
 
@@ -49,25 +52,41 @@ public record class GoogleSearch : SearchService {
 
     private async Task<PossibleAttributesProduct?> CompletePossibleAttributesProductAsync(Google.Apis.CustomSearchAPI.v1.Data.Result item) {
         PossibleAttributesProduct possibleAttributes = new() {
-            SiteName = item.Title 
+            WebAddress = new Uri(item.FormattedUrl),
+            SiteName = item.Title
         };
 
         String response = await GetResponseHtmlFromWebsiteAsync(item.Link);
-
-        var matchCollection = Regex.Matches(response, PatternImgSrc);
-        possibleAttributes.UriImages = matchCollection
-            .Select(s 
-                => Uri.TryCreate(s.Groups[1].Value, UriKind.RelativeOrAbsolute, out var uriResult) == true ? uriResult : null)
-            .Where(s => s is not null)
-            .ToList();
-
         CQ domObjects = new CQ(response);
+
+        var aboutProduct = new AboutProduct() {
+            Description = await GetInvariantsDescriptionsAsync(domObjects),
+            Specifity = new Dictionary<string, IConvertible>()
+        };
+
+        //Images
         possibleAttributes.UriImages = domObjects["img"]
             .Select((d, i) => d.GetAttribute("src"))
-            .Select(s => new Uri(s))
+            .Select(s => Uri.TryCreate(s, UriKind.RelativeOrAbsolute, out var uriResult) == true ? uriResult : new Uri(""))
+            .Select(u => u.IsAbsoluteUri == false ? new Uri(possibleAttributes.WebAddress, u) : u)
             .ToList();
 
         return possibleAttributes;
+    }
+
+    private async Task<List<String>> GetInvariantsDescriptionsAsync(CQ domObjects) {
+        List<String> result = new List<String>();
+        foreach (var domObject in domObjects["div"]) {
+            var item = domObject.GetAttribute("itemprop");
+            if (item is null)
+                continue;
+
+            if (item is String itemprop && itemprop.ToLower() == "description") {
+                result.Add(domObject.Value);
+            }
+        }
+
+        return result;
     }
 
     private async Task<String> GetResponseHtmlFromWebsiteAsync(String uriWebsite) {
@@ -82,8 +101,14 @@ public record class GoogleSearch : SearchService {
     }
 
     public record struct PossibleAttributesProduct {
+        public Uri WebAddress { get; set; }
         public String SiteName { get; init; }
-        public List<String> Text { get; set; }
         public List<Uri> UriImages { get; set; }
+        public AboutProduct AboutProduct { get; set; }
+    }
+
+    public record struct AboutProduct {
+        public List<String> Description { get; set; }
+        public Dictionary<String, IConvertible> Specifity { get; set; }
     }
 }
