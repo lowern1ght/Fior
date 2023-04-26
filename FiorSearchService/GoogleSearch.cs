94bf8855ed941d8b393413b615396514f0464c50
Alexander Sergeev <bradwoord@gmail.com>
2023-04-27 00:49:42 +0500

    #5001alp

--- FiorSearchService/GoogleSearch.cs
+++ FiorSearchService/GoogleSearch.cs
@@ -9,20 +9,73 @@ using CsQuery;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using System.Runtime.InteropServices;
+using OpenQA.Selenium.Firefox;
+using CsQuery.ExtensionMethods;
 
 namespace FiorSearchService;
 
 public record class GoogleSearch : SearchService {
     private LogService LogService { get; init; }
-    private EdgeDriver EdgeDriver { get; init; }
+    private IWebDriver WebDriver { get; init; }
     public CustomSearchAPIService CustomSearch { get; init; }
 
     private readonly string[] ExtensionImage = new string[] { ".jpeg", ".png", ".jpg" };
     private const string PatternImgSrc = @"<img\s[^>]*?src\s*=\s*['\""]([^'\""]*?)['\""][^>]*?>";
 
-    public GoogleSearch(SearchServiceConfig serviceConfig) : base(serviceConfig) {
-        this.EdgeDriver = new EdgeDriver() { };
+    ~GoogleSearch() {
+        WebDriver.Quit();
+        WebDriver.Dispose();
+    }
+
+    public GoogleSearch(SearchServiceConfig serviceConfig, IWebDriver? webDriver = null) : base(serviceConfig) {
         LogService = new LogService(LoggingTo.Console);
+
+        if (webDriver is null) {
+            switch (Environment.OSVersion.Platform) {
+                case PlatformID.Unix: {
+                        var optionFirefox = new FirefoxOptions() {
+                            AcceptInsecureCertificates = true,
+                        };
+
+                        var serviceFirefox = FirefoxDriverService.CreateDefaultService();
+                        serviceFirefox.SuppressInitialDiagnosticInformation = true;
+                        serviceFirefox.HideCommandPromptWindow = true;
+
+                        optionFirefox.AddArgument(@"--disable-gpu");
+                        optionFirefox.AddArgument(@"--log-level=3" );
+                        optionFirefox.AddArgument(@"--output=/dev/null");
+                        optionFirefox.AddArgument(@"--disable-crash-reporter");
+
+                        WebDriver = new FirefoxDriver(serviceFirefox, optionFirefox);
+                        break;
+                    }
+                case PlatformID.Win32NT: {
+                        var optionEdge = new EdgeOptions() {
+                            AcceptInsecureCertificates = true,
+                        };
+
+                        var serviceEdge = EdgeDriverService.CreateDefaultService();
+                        serviceEdge.SuppressInitialDiagnosticInformation = true;
+                        serviceEdge.HideCommandPromptWindow = true;
+                        serviceEdge.UseVerboseLogging = false;
+
+                        optionEdge.AddArgument(@"--disable-gpu");
+                        optionEdge.AddArgument(@"--log-level=3");
+                        optionEdge.AddArgument(@"--output=/dev/null");
+                        optionEdge.AddArgument(@"--disable-extensions");
+                        optionEdge.AddArgument(@"--disable-crash-reporter");
+
+                        WebDriver = new EdgeDriver(serviceEdge,  optionEdge);
+                        break;
+                    }
+                default: {
+                        throw new NotSupportedException();
+                    }
+            }
+        }
+        else { WebDriver = webDriver; }
+
         CustomSearch = new(
             new Google.Apis.Services.BaseClientService.Initializer() {
                 ApiKey = ServiceConfig.ApiKey,
@@ -87,6 +140,16 @@ public record class GoogleSearch : SearchService {
         return possibleAttributes;
     }
 
+    public async Task AddRecursedValueAsync(IDomObject @object, IList<String> text) {
+        if (@object.HasChildren) {
+            foreach (var node in @object.ChildNodes) {
+                await AddRecursedValueAsync(node, text);
+            }
+        } else {
+            text.Add(@object.Value);
+        }
+    }
+
     private async Task<List<String>> GetInvariantsDescriptionsAsync(CQ domObjects) {
         List<string> result = new List<string>();
         foreach (var domObject in domObjects["div"]) {
@@ -95,19 +158,35 @@ public record class GoogleSearch : SearchService {
                 continue;
 
             if (item is string itemprop && itemprop.ToLower() == "description") {
+                switch (itemprop.ToLower()) {
+                    case "description": {
+                            await AddRecursedValueAsync(domObject, result);
+                            break;
+                        }
+                }
+
+
+
+
+
                 var value = domObject.Value;
                 await LogService.Log("Added description: {0}", Modules.LogType.Info, value);
                 result.Add(domObject.Value);
+            } else {
+                continue;
             }
         }
+
+        //foreach (var domObject in domObjects["t"])
 
         return result;
     }
 
     private async Task<String?> GetResponseHtmlFromWebsite(Uri uriWebsite) {
         try {
-            EdgeDriver.Navigate().GoToUrl(uriWebsite);
-            return EdgeDriver.PageSource;
+            WebDriver.Navigate().GoToUrl(uriWebsite);
+            await LogService.Log("WebSite: {0}, Status: {Open}", Modules.LogType.Info, uriWebsite.ToString(), "Open");
+            return WebDriver.PageSource;
         }
         catch (WebDriverException ex) {
             await LogService.Log(ex.Message, Modules.LogType.Errored, ex);
