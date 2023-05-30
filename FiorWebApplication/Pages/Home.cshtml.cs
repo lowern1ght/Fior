3262682fd9c7ee602e0075c8ba001e58c96ecc11
buw <bradwoord@gmail.com>
2023-05-30 16:32:30 +0500

    #new project web service

--- FiorWebApplication/Pages/Home.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace FiorWebApplication.Pages; 
-
-public class Home : PageModel {
-    public void OnGet() {
-        Response.WriteAsync("dawdawd");
-    }
-}
